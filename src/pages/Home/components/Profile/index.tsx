ad402c9da170f4f53ded8ee57009d393939d1c72
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-09 17:02:49 -0300

    Profile card

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,9 +1,55 @@
-import { ProfileContainer } from './styles'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBuilding,
+  faUserGroup,
+  faUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  ProfileContainer,
+  ProfileDetails,
+  ProfileHeader,
+  ProfileIcons,
+} from './styles'
 
 export function Profile() {
   return (
     <ProfileContainer>
-      <h1>Profile</h1>
+      <img src="http://github.com/milsucruz.png" alt="Foto do Edmilson Cruz" />
+
+      <ProfileDetails>
+        <ProfileHeader>
+          <h2>Edmilson Cruz</h2>
+          <a
+            href="http://github.com/milsucruz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+            <FontAwesomeIcon icon={faUpRightFromSquare} fontSize={12} />
+          </a>
+        </ProfileHeader>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi qui
+          amet impedit corporis? Ut nobis atque commodi provident libero iure
+          explicabo. Vitae sit, dolor corrupti quos illum nam doloremque totam?
+        </p>
+
+        <ProfileIcons>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            milsucruz
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            Rocketseat
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserGroup} />
+            32 seguidores
+          </li>
+        </ProfileIcons>
+      </ProfileDetails>
     </ProfileContainer>
   )
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -1,3 +1,70 @@
 import { styled } from 'styled-components'
 
-export const ProfileContainer = styled.div``
+export const ProfileContainer = styled.div`
+  display: flex;
+
+  gap: 2rem;
+  padding: 2rem 2rem 2rem 2.5rem;
+  margin-top: -6.5rem;
+
+  background: ${(props) => props.theme['base-profile']};
+
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+
+  img {
+    width: 9.25rem;
+    border-radius: 8px;
+  }
+`
+
+export const ProfileDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+export const ProfileHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${(props) => props.theme['base-title']};
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 0.75rem;
+    line-height: normal;
+    gap: 0.5rem;
+    text-transform: uppercase;
+
+    color: ${(props) => props.theme.blue};
+
+    /* border-top: 1px solid transparent; */
+    border-bottom: 1px solid transparent;
+  }
+
+  a:hover {
+    border-bottom: 1px solid ${(props) => props.theme.blue};
+  }
+`
+
+export const ProfileIcons = styled.ul`
+  display: flex;
+  gap: 1.5rem;
+
+  list-style: none;
+
+  color: ${(props) => props.theme['base-subtitle']};
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  svg {
+    opacity: 0.4;
+    font-size: 1.125rem;
+  }
+`
