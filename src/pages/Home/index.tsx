2c12f57ee90b48c2faeb008f154ea816c813e0d6
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-12 15:12:37 -0300

    Home API-Github

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,59 @@
+import { useCallback, useEffect, useState } from 'react'
 import { PostCard } from './components/PostCard'
 import { Profile } from './components/Profile'
 import { SearchForm } from './components/SearchForm'
 import { HomeContainer, PostsCardsContainer, PostsCardsList } from './styles'
+import { api } from '../../lib/axios'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPONAME
+
+export interface IPost {
+  title: string
+  body: string
+  created_at: string
+  number: number
+  html_url: string
+  comments: number
+  user: {
+    login: string
+  }
+}
 
 export function Home() {
+  const [posts, setPosts] = useState<IPost[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getPosts = useCallback(
+    async (query: string = '') => {
+      try {
+        setIsLoading(true)
+        const response = await api.get(
+          `/search/issues?q=${query}%20repo:${username}/${repoName}`,
+        )
+
+        console.log(response.data)
+        setPosts(response.data.items)
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [posts],
+  )
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <HomeContainer>
       <Profile />
       <PostsCardsContainer>
         <SearchForm />
         <PostsCardsList>
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
+          {posts.map((post) => (
+            <PostCard key={post.number} post={post} />
+          ))}
         </PostsCardsList>
       </PostsCardsContainer>
     </HomeContainer>
