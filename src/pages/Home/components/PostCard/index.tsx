38cb30ff917a162af81e73703645f4ed643138fb
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-06-12 10:14:52 -0300

    NavLink

--- src/pages/Home/components/PostCard/index.tsx
+++ src/pages/Home/components/PostCard/index.tsx
@@ -1,20 +1,24 @@
+import { NavLink } from 'react-router-dom'
 import { PostCardContent } from './styles'
 
 export function PostCard() {
   return (
-    <PostCardContent>
-      <div>
-        <h2>JavaScript data types and data structures</h2>
-        <span>Há 1 dia</span>
-      </div>
+    <NavLink to="/posts">
+      <PostCardContent>
+        <div>
+          <h2>JavaScript data types and data structures</h2>
+          <span>Há 1 dia</span>
+        </div>
 
-      <p>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in JavaScript and what properties
-        they have. These can be used to build other data structures. Wherever
-        possible, comparisons with other languages are drawn.
-      </p>
-    </PostCardContent>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures. Wherever possible, comparisons with other languages are
+          drawn.
+        </p>
+      </PostCardContent>
+    </NavLink>
   )
 }
